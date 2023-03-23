b765efbf1a37abca686a122c909f4fe1d41bf898
Wiktor <wijaweq@gmail.com>
2023-03-23 23:55:31 +0100

    Server prototype

---
+++ WZIMpolyServer/Program.cs
@@ -0,0 +1,32 @@
+﻿#region Using Statements
+using WebSocketSharp.Server;
+#endregion
+
+namespace WZIMpolyServer
+{
+    internal class Server
+    {
+        /// <value>
+        /// Represents a <c>WebSocket</c> server.
+        /// </value>
+        internal static WebSocketServer wssv = new("ws://0.0.0.0:8765");
+
+        /// <value>
+        /// Represents a list of active lobby codes.
+        /// </value>
+        internal static List<string> LobbyCodes = new();
+
+        /// <summary>
+        /// Starts the server.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            wssv.AddWebSocketService<Root>("/");
+            wssv.Start();
+            Console.WriteLine("Server started. Press any key to stop.");
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
