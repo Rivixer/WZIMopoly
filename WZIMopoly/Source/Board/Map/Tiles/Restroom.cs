e3aeafeb9528205793d26a2015c6a6a87c3b16ca
Natalia Konopka <s210876@sggw.edu.pl>
2023-04-02 15:21:06 +0200

    Fix doc of classes representing tiles

--- WZIMopoly/Source/Board/Map/Tiles/Restroom.cs
+++ WZIMopoly/Source/Board/Map/Tiles/Restroom.cs
@@ -4,6 +4,16 @@ using System.Xml;
 
 namespace WZIMopoly.Source.Board.Map.Tiles
 {
+    /// <summary>
+    /// Represents a 'Restroom' tile.<br/>
+    /// This is a purchasable tile. <br/>
+    /// If player steps on this field, they have to pay a rent to the person who owns this tile.
+    /// There is a chance to pop an owned tile. It's about pawning the card in the bank for a 
+    /// certain amount of the ECTS points.
+    /// Money from someone entering this tile is no rewarded during this stage. <br/>
+    /// In our game, bathroom is the equivalent of one of the Monopoly 
+    /// <see href="https://monopoly.fandom.com/wiki/Railroads">'Railroads'</see>.<br/>
+    /// </summary>
     class Restroom : PurchasableTile
     {
         public Restroom(XmlNode node) : base(node)
