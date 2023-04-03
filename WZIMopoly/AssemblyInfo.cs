df69d3e9eed6cab140e62a67d268f4af8bd6a02a
Natalia Konopka <s210876@sggw.edu.pl>
2023-04-04 00:14:16 +0200

    Add AssemblyInfo.cs

---
+++ WZIMopoly/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+
+// W projektach w stylu zestawu SDK, takich jak ten, kilka atrybutów zestawu, kiedyś
+// definiowanych w tym pliku, jest teraz automatycznie dodawanych podczas kompilacji
+// i wypełnianych wartościami zdefiniowanymi we właściwościach projektu. Aby uzyskać
+// szczegółowe informacje o tym, które atrybuty są dołączane i jak dostosować ten
+// proces, zobacz: https://aka.ms/assembly-info-properties
+
+
+// Ustawienie atrybutu ComVisible na wartość false sprawia, że typy w tym zestawie
+// nie są widoczne dla składników modelu COM. Jeśli chcesz uzyskać dostęp do typu
+// w tym zestawie z modelu COM, ustaw w tym typie atrybut ComVisible na wartość true.
+
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem elementu typelib w przypadku
+// udostępnienia tego projektu w modelu COM.
+
+[assembly: Guid("d7f6082c-8152-4e15-8dbc-4e04772fb8b4")]
