b34a2c8cbdfbd40cc3d4782225196529ceae41ef
paulinassss <128840626+paulinassss@users.noreply.github.com>
2023-04-05 23:47:48 +0200

    Modify Player class

--- WZIMopoly/Source/Player/Player.cs
+++ WZIMopoly/Source/Player/Player.cs
@@ -2,5 +2,26 @@
 {
     public class Player
     {
+        public readonly string Color;
+        private string _nick;
+        private Tile _tile;
+        
+        public Player(string nick, string color)
+        {
+            _nick = nick;
+            Color = color;
+        }
+
+        public string Nick
+        {
+            get => _nick;
+            set => _nick = value;
+        }
+
+        public Tile Tile
+        {
+            get => _tile;
+            set => _tile = value;
+        }
     }
 }
