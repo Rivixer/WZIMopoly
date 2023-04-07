042cb4720b1b29cec32d498b664aac5d4241844c
Jakub Kostrzewa <kubacfaniak@gmail.com>
2023-04-07 18:34:20 +0200

    Add documentation

--- WZIMopoly/Source/Engine/MouseController.cs
+++ WZIMopoly/Source/Engine/MouseController.cs
@@ -9,23 +9,39 @@ namespace WZIMopoly
     {
         private static MouseState _mouse = Mouse.GetState();
         private static MouseState _oldMouse;
+
+        /// <summary>
+        /// Updates the state of mouse
+        /// </summary>
         public static void Update()
         {
             _oldMouse = _mouse;
             _mouse = Mouse.GetState();
         }
+
+        /// <summary>
+        /// Checks if left mouse button was clicked
+        /// </summary>
         public static bool WasLeftBtnClicked()
         {
             bool wasClicked = (_oldMouse.LeftButton == ButtonState.Pressed);
             bool isClicked = (_mouse.LeftButton == ButtonState.Pressed);
             return wasClicked && !isClicked;
         }
+
+        /// <summary>
+        /// Checks if left mouse button is being pressed
+        /// </summary>
         public static bool IsLeftBtnPressed()
         {
             bool wasClicked = (_oldMouse.LeftButton == ButtonState.Pressed);
             bool isClicked = (_mouse.LeftButton == ButtonState.Pressed);
             return wasClicked && isClicked;
         }
+
+        /// <summary>
+        /// Checks if left mouse button was released
+        /// </summary>
         public static bool WasLeftBtnReleased()
         {
 
@@ -33,6 +49,10 @@ namespace WZIMopoly
             bool isClicked = (_mouse.LeftButton == ButtonState.Pressed);
             return wasClicked && !isClicked;
         }
+
+        /// <summary>
+        /// Checks if mouse position is within rectangle
+        /// </summary>
         public static bool IsHover(Rectangle rect)
         {
             return rect.Contains(_mouse.Position);
