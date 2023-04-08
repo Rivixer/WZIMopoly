8ebfb381f01843512e7a8040abbbbc600ab84d09
Zetka <jakub.kochanski10@gmail.com>
2023-04-08 22:03:14 +0200

    Added interfaces

---
+++ WZIMopoly/Source/GUI/IGUIDrawable.cs
@@ -0,0 +1,17 @@
+﻿#region Using Statements
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace WZIMopoly.GUI
+{
+    internal interface IGUIDrawable
+    {
+        /// <summary>
+        /// Draws the screen.
+        /// </summary>
+        /// <param name="spriteBatch">
+        /// The SpriteBatch object used for rendering.
+        /// </param>
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
