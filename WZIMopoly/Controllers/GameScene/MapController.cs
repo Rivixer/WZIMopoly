7ffd6abd3654fad24c6f9795c463ed54a8cbf6bd
Wiktor Jaworski <wijaweq@gmail.com>
2023-04-11 22:41:01 +0200

    Create MapController with model and view

---
+++ WZIMopoly/Controllers/GameScene/MapController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Xml;
+using WZIMopoly.GUI.GameScene;
+using WZIMopoly.Models.GameScreen;
+
+namespace WZIMopoly.Controllers.GameScene
+{
+    /// <summary>
+    /// Represents a controller of the map.
+    /// </summary>
+    internal sealed class MapController : Controller
+    {
+        /// <summary>
+        /// Gets the model of the map controller.
+        /// </summary>
+        private new MapModel Model => (MapModel)base.Model;
+
+        /// <summary>
+        /// Initilizes a new instance of the <see cref="MapController"/> class.
+        /// </summary>
+        /// <remarks>
+        /// Loads tiles from a xml file calling <see cref="LoadTiles()"/> method.
+        /// </remarks>
+        internal MapController(MapView view, MapModel model)
+            : base(view, model, false)
+        {
+            LoadTiles();
+        }
+
+        /// <summary>
+        /// Loads tiles from a xml file.
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        private void LoadTiles()
+        {
+            var TilesXml = new XmlDocument();
+            TilesXml.Load("../../../Properties/Tiles.xml");
+
+            string namespacePrefix = "WZIMopoly.Controllers.GameScene.Tiles";
+            foreach (XmlNode TileNode in TilesXml.DocumentElement.ChildNodes)
+            {
+                string RawTileType = TileNode.Attributes["type"].Value;
+                Type TileType = Type.GetType($"{namespacePrefix}.{RawTileType}");
+
+                if (TileType != null)
+                {
+                    Tile tile = (Tile)Activator.CreateInstance(TileType, TileNode);
+                    Model.Tiles.Add(tile);
+                }
+                else
+                {
+                    throw new ArgumentException($"Invalid value of type attribute: {RawTileType}; " +
+                        $"in tile node with {TileNode.Attributes["id"].Value} id in xml file.");
+                }
+            }
+        }
+    }
+}
---
+++ WZIMopoly/GUI/GameScene/MapView.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WZIMopoly.GUI.GameScene
+{
+    /// <summary>
+    /// Represents a map view.
+    /// </summary>
+    internal sealed class MapView : GUIElement
+    {
+        internal override void Load(ContentManager content)
+        {
+            Texture = null; // TODO: Load the board texture here
+        }
+
+        internal override void Draw(SpriteBatch spriteBatch)
+        {
+            // TODO: Remove this override after load the texture
+        }
+    }
+}
