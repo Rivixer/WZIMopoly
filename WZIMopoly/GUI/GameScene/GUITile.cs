b1752ae10d0c94fb2f1b3f1ffa77478a0092dd77
Damian Wujkowski <gewo556@gmail.com>
2023-04-15 01:51:06 +0200

    Create class

---
+++ WZIMopoly/GUI/GameScene/GUITile.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using WZIMopoly.Controllers.GameScene;
+using Microsoft.Xna.Framework;
+
+namespace WZIMopoly.GUI.GameScene
+{
+    internal sealed class GUITile : GUIText
+    {
+        private readonly Tile _tile;
+
+        internal GUITile(Tile tile) : base(Vector2.Zero)
+        {
+            _tile = tile;
+        }
+
+        internal override void Load(ContentManager content)
+        {
+            Font = content.Load<SpriteFont>("Fonts/WZIMFont");
+        }
+    }
+}
