57ae571c5d3a51ea1e3fb56ce590b515bf08bdce
Damian Wujkowski <gewo556@gmail.com>
2023-04-29 02:26:04 +0200

    Change console image and create initialization func to Scene.cs

--- WZIMopoly/Controllers/Controller.cs
+++ WZIMopoly/Controllers/Controller.cs
@@ -20,7 +20,7 @@ namespace WZIMopoly
         /// <summary>
         /// The list of children of the controller.
         /// </summary>
-        public List<Controller> Children;
+        protected List<Controller> Children;
 
         /// <summary>
         /// Whether the controller is a primary one and has no parents.
