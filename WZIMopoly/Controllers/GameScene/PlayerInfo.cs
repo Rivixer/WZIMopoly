80fc41197070579309c1f8b9315146f1e79c1fbc
Wiktor Jaworski <wijaweq@gmail.com>
2023-05-03 21:24:33 +0200

    Improve comments

--- WZIMopoly/Controllers/GameScene/PlayerInfo.cs
+++ WZIMopoly/Controllers/GameScene/PlayerInfo.cs
@@ -4,14 +4,23 @@ using WZIMopoly.Models.GameScene;
 namespace WZIMopoly.Controllers.GameScene
 {
     /// <summary>
-    /// Represents a controller for PlayerInfo.
+    /// Represents a controller for player information.
     /// </summary>
-    /// /// <remarks>
+    /// <remarks>
     /// Used to display player information such as
     /// nickname or amount of money in the game scene.
-    /// <remarks>
+    /// </remarks>
     internal class PlayerInfo : Controller<PlayerInfoModel, GUIPlayerInfo>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayerInfo"/> class.
+        /// </summary>
+        /// <param name="model">
+        /// The model of the player information.
+        /// </param>
+        /// <param name="view">
+        /// The view of the player information.
+        /// </param>
         internal PlayerInfo(PlayerInfoModel model, GUIPlayerInfo view)
             : base (model, view) { }
     }
--- WZIMopoly/Models/GameScene/PlayerInfoModel.cs
+++ WZIMopoly/Models/GameScene/PlayerInfoModel.cs
@@ -8,12 +8,36 @@ namespace WZIMopoly.Models.GameScene
     /// </summary>
     internal class PlayerInfoModel : Model
     {
+        /// <summary>
+        /// Gets or privately sets the player model.
+        /// </summary>
         internal Player Player { get; private set; }
 
+        /// <summary>
+        /// Gets or privately sets the default rectangle of the player information texture.
+        /// </summary>
         internal Rectangle DefRectangle { get; private set; }
 
+        /// <summary>
+        /// Gets or privately sets the GUI start point of the player information texture.
+        /// </summary>
+        /// <remarks>
+        /// Used to determine the place for which <see cref="DefRectangle"/> is specified.
+        /// </remarks>
         internal GUIStartPoint StartPoint { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayerInfoModel"/> class.
+        /// </summary>
+        /// <param name="player">
+        /// The player model.
+        /// </param>
+        /// <param name="defRect">
+        /// The default rectangle of the player information texture.
+        /// </param>
+        /// <param name="startPoint">
+        /// The start point that determines the place for which <paramref name="defRect"> is specified.
+        /// </param>
         internal PlayerInfoModel(Player player, Rectangle defRect, GUIStartPoint startPoint)
         {
             Player = player;
