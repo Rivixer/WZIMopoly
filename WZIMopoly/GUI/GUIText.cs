4e004b48a5a98377adcf2666fc38a48f1374b288
Wiktor Jaworski <wijaweq@gmail.com>
2023-05-03 13:59:20 +0200

    Make GUITexture and GUIText non-abstract

--- WZIMopoly/GUI/GUIText.cs
+++ WZIMopoly/GUI/GUIText.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using WZIMopoly.Engine;
 using WZIMopoly.Enums;
@@ -8,21 +9,13 @@ namespace WZIMopoly.GUI
     /// <summary>
     /// Represents a text GUI element.
     /// </summary>
-    internal abstract class GUIText : GUIElement
+    internal class GUIText : GUIElement
     {
         #region Fields
         /// <summary>
         /// The font of the text.
         /// </summary>
         protected SpriteFont Font;
-
-        /// <summary>
-        /// The vector that represents the position scaled to the current screen resolution.
-        /// </summary>
-        /// <remarks>
-        /// The X and Y coordinates refer to the position <see cref="_startPoint"/> of the element.
-        /// </remarks>
-        protected Vector2 Position;
 
         /// <summary>
         /// The color of the text.
@@ -35,17 +28,22 @@ namespace WZIMopoly.GUI
         protected float Scale;
 
         /// <summary>
-        /// The background of the text.
-        /// </summary>
-        /// <remarks>
-        /// If the background is null, the text will be displayed without background.
-        /// </remarks>
-        protected GUITexture Background;
-
-        /// <summary>
         /// The text to display.
         /// </summary>
         private string _text;
+
+        /// <summary>
+        /// The vector that represents the position scaled to the current screen resolution.
+        /// </summary>
+        /// <remarks>
+        /// The X and Y coordinates refer to the position <see cref="_startPoint"/> of the element.
+        /// </remarks>
+        private Vector2 _position;
+
+        /// <summary>
+        /// The path to the font.
+        /// </summary>
+        private readonly string _fontPath;
 
         /// <summary>
         /// The scale of the text specified for 1920x1080 resolution.
@@ -76,11 +74,19 @@ namespace WZIMopoly.GUI
         /// <param name="startPoint">
         /// The starting position of the element for which <paramref name="defPosition"/> has been specified.
         /// </param>
+        /// <param name="text">
+        /// The text to display.<br/>
+        /// Defaults to empty string.
+        /// </param>
+        /// <param name="scale">
+        /// The scale of the text. <br/>
+        /// Defaults to 1.0f.
+        /// </param>
         /// <remarks>
         /// The <see cref="Color"/> property is set to white and the <see cref="Text"/> property is empty by default.
         /// </remarks>
-        protected GUIText(Vector2 defPosition, GUIStartPoint startPoint = GUIStartPoint.TopLeft)
-            : this(defPosition, startPoint, Color.White) { }
+        internal GUIText(string fontPath, Vector2 defPosition, GUIStartPoint startPoint = GUIStartPoint.TopLeft, string text = "", float scale = 1.0f)
+            : this(fontPath, defPosition, startPoint, Color.White, text, scale) { }
 
         /// <summary>
         /// Initializes a new instance of <see cref="GUIText"/> class.
@@ -102,8 +108,9 @@ namespace WZIMopoly.GUI
         /// The scale of the text. <br/>
         /// Defaults to 1.0f.
         /// </param>
-        protected GUIText(Vector2 defPosition, GUIStartPoint startPoint, Color color, string text = "", float scale = 1.0f)
+        internal GUIText(string fontPath, Vector2 defPosition, GUIStartPoint startPoint, Color color, string text = "", float scale = 1.0f)
         {
+            _fontPath = fontPath;
             _defaultPosition = defPosition;
             _startPoint = startPoint;
             _defaultScale = scale;
@@ -112,9 +119,13 @@ namespace WZIMopoly.GUI
         }
         #endregion
 
+        #region Properties
         /// <summary>
         /// Gets or sets the text of the element.
         /// </summary>
+        /// <value>
+        /// The text to display.
+        /// </value>
         protected string Text
         {
             get => _text;
@@ -125,21 +136,31 @@ namespace WZIMopoly.GUI
             }
         }
 
+        /// <summary>
+        /// Gets the position of the text.
+        /// </summary>
+        /// <value>
+        /// The position of the text relative to the top-left corner.
+        /// </value>
+        protected Vector2 Position => _position;
+        #endregion
+
+        #region GUIElement Methods
         /// <inheritdoc/>
         internal override void Draw(SpriteBatch spriteBatch)
         {
-            Background?.Draw(spriteBatch);
             if (Font is not null)
             {
-                spriteBatch.DrawString(Font, Text, Position, Color, 0, Vector2.Zero, Scale, SpriteEffects.None, 0f);
+                spriteBatch.DrawString(Font, Text, _position, Color, 0, Vector2.Zero, Scale, SpriteEffects.None, 0f);
             }
+            base.Draw(spriteBatch);
         }
 
         /// <summary>
         /// Scales <see cref="_defaultPosition"/> and <see cref="_defaultScale"/> for the current screen resolution.<br/>
         /// </summary>
         /// <remarks>
-        /// Saves it to <see cref="Position"/> and <see cref="Scale"/> field.
+        /// Saves it to <see cref="_position"/> and <see cref="Scale"/> field.
         /// </remarks>
         internal override void Recalculate()
         {
@@ -148,11 +169,11 @@ namespace WZIMopoly.GUI
                 return;
             }
 
-            var x = _defaultPosition.X * ScreenController.Width / 1920;
-            var y = _defaultPosition.Y * ScreenController.Height / 1080;
-
-            float textWidth = Font.MeasureString(Text).X * Scale;
-            float textHeight = Font.MeasureString(Text).Y * Scale;
+            Scale = _defaultScale * ScreenController.Height / 1080;
+            int textWidth = (int)(Font.MeasureString(Text).X * Scale);
+            int textHeight = (int)(Font.MeasureString(Text).Y * Scale);
+            float x = _defaultPosition.X * ScreenController.Width / 1920;
+            float y = _defaultPosition.Y * ScreenController.Height / 1080;
 
             switch (_startPoint)
             {
@@ -188,9 +209,25 @@ namespace WZIMopoly.GUI
                     break;
             }
 
-            Position = new Vector2(x, y);
-            Scale = _defaultScale * ScreenController.Height / 1080;
-            Background?.Recalculate();
+            _position = new Vector2(x, y);
+            base.Recalculate();
         }
+
+        /// <summary>
+        /// Loads the font.
+        /// </summary>
+        /// <remarks>
+        /// After loading the font, <see cref="Recalculate"/> is called.
+        /// </remarks>
+        /// <param name="content">
+        /// The ContentManager used to load the font.
+        /// </param>
+        internal override void Load(ContentManager content)
+        {
+            Font = content.Load<SpriteFont>(_fontPath);
+            Recalculate();
+            base.Load(content);
+        }
+        #endregion
     }
 }
