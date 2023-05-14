561727553c3fc1b6d1cf0b40c8c2a8c511c1e9fe
Zetka <jakub.kochanski10@gmail.com>
2023-05-14 17:30:57 +0200

    Added dices to screen.

--- WZIMopoly/GUI/GameScene/GUIDice.cs
+++ WZIMopoly/GUI/GameScene/GUIDice.cs
@@ -1,14 +1,57 @@
-﻿using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using WZIMopoly.Enums;
+using WZIMopoly.Models.GameScene;
 
 namespace WZIMopoly.GUI.GameScene
 {
     internal class GUIDice : GUIElement
     {
-        internal override void Draw(SpriteBatch spriteBatch) { }
+        /// <summary>
+        /// List of textures for the first dice.
+        /// </summary>
+        private readonly List<GUITexture> _firstDiceTextures;
+        /// <summary>
+        /// List of textures for the second dice.
+        /// </summary>
+        private readonly List<GUITexture> _secondDiceTextures;
+        private readonly DiceModel _model;
 
-        internal override void Load(ContentManager content) { }
+        internal GUIDice(DiceModel model)
+        {
+            _model = model;
+            _firstDiceTextures = new List<GUITexture>();
+            _secondDiceTextures = new List<GUITexture>();
+            for (int i = 0; i < 6; i++)
+            {
+                var text = $"images\\dice{i + 1}";
+                _firstDiceTextures.Add(new GUITexture(text, new Rectangle(660, 540, 160, 160),GUIStartPoint.Center));
+                _secondDiceTextures.Add(new GUITexture(text, new Rectangle(1260, 540, 160, 160),GUIStartPoint.Center));
+            }
+        }
 
-        internal override void Recalculate() { }
+        /// <summary>
+        /// Draws dices to the screen.
+        /// </summary>
+        internal override void Draw(SpriteBatch spriteBatch)        
+        {
+            if(_model.LastRoll != null)
+            {
+                _firstDiceTextures[_model.LastRoll.Item1-1].Draw(spriteBatch);
+                _secondDiceTextures[_model.LastRoll.Item2-1].Draw(spriteBatch);
+            }
+        }
+
+        internal override void Load(ContentManager content) {
+            _firstDiceTextures.ForEach(x => x.Load(content));
+            _secondDiceTextures.ForEach(x => x.Load(content));
+        }
+
+        internal override void Recalculate() {
+            _firstDiceTextures.ForEach(x=>x.Recalculate());
+            _secondDiceTextures.ForEach(x=>x.Recalculate());
+        }
     }
 }
