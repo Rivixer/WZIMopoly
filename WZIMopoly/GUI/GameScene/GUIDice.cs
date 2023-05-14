fea011e05c029944ff4a9ad5820e4a3844f79ef5
Wiktor Jaworski <wijaweq@gmail.com>
2023-05-14 18:47:57 +0200

    Format code in GUIDice

--- WZIMopoly/GUI/GameScene/GUIDice.cs
+++ WZIMopoly/GUI/GameScene/GUIDice.cs
@@ -7,51 +7,73 @@ using WZIMopoly.Models.GameScene;
 
 namespace WZIMopoly.GUI.GameScene
 {
+    /// <summary>
+    /// Represents the dice GUI element.
+    /// </summary>
+    /// <remarks>
+    /// Shows the last roll of the dice on the screen.
+    /// </remarks>
     internal class GUIDice : GUIElement
     {
         /// <summary>
         /// List of textures for the first dice.
         /// </summary>
         private readonly List<GUITexture> _firstDiceTextures;
+
         /// <summary>
         /// List of textures for the second dice.
         /// </summary>
         private readonly List<GUITexture> _secondDiceTextures;
+
+        /// <summary>
+        /// The model of the dice.
+        /// </summary>
         private readonly DiceModel _model;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GUIDice"/> class.
+        /// </summary>
+        /// <param name="model">
+        /// The model of the dice.
+        /// </param>
         internal GUIDice(DiceModel model)
         {
             _model = model;
             _firstDiceTextures = new List<GUITexture>();
             _secondDiceTextures = new List<GUITexture>();
+
+            var rect1 = new Rectangle(660, 540, 160, 160);
+            var rect2 = new Rectangle(1260, 540, 160, 160);
             for (int i = 0; i < 6; i++)
             {
-                var text = $"images\\dice{i + 1}";
-                _firstDiceTextures.Add(new GUITexture(text, new Rectangle(660, 540, 160, 160),GUIStartPoint.Center));
-                _secondDiceTextures.Add(new GUITexture(text, new Rectangle(1260, 540, 160, 160),GUIStartPoint.Center));
+                var text = $"Images\\Dice{i + 1}";
+                _firstDiceTextures.Add(new GUITexture(text, rect1, GUIStartPoint.Center));
+                _secondDiceTextures.Add(new GUITexture(text, rect2, GUIStartPoint.Center));
             }
         }
 
-        /// <summary>
-        /// Draws dices to the screen.
-        /// </summary>
-        internal override void Draw(SpriteBatch spriteBatch)        
+        /// <inheritdoc/>
+        public override void Draw(SpriteBatch spriteBatch)
         {
-            if(_model.LastRoll != null)
+            if (_model.LastRoll != null)
             {
-                _firstDiceTextures[_model.LastRoll.Item1-1].Draw(spriteBatch);
-                _secondDiceTextures[_model.LastRoll.Item2-1].Draw(spriteBatch);
+                _firstDiceTextures[_model.LastRoll.Item1 - 1].Draw(spriteBatch);
+                _secondDiceTextures[_model.LastRoll.Item2 - 1].Draw(spriteBatch);
             }
         }
 
-        internal override void Load(ContentManager content) {
+        /// <inheritdoc/>
+        public override void Load(ContentManager content)
+        {
             _firstDiceTextures.ForEach(x => x.Load(content));
             _secondDiceTextures.ForEach(x => x.Load(content));
         }
 
-        internal override void Recalculate() {
-            _firstDiceTextures.ForEach(x=>x.Recalculate());
-            _secondDiceTextures.ForEach(x=>x.Recalculate());
+        /// <inheritdoc/>
+        public override void Recalculate()
+        {
+            _firstDiceTextures.ForEach(x => x.Recalculate());
+            _secondDiceTextures.ForEach(x => x.Recalculate());
         }
     }
 }
