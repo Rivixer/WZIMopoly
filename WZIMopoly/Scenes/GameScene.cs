4f007e44a2ca63cd9d8f8d8063c7f905cb8ac93e
Jakub Kostrzewa <kubacfaniak@gmail.com>
2023-05-16 18:20:15 +0200

    change position of players so they move clockwise

--- WZIMopoly/Scenes/GameScene.cs
+++ WZIMopoly/Scenes/GameScene.cs
@@ -49,9 +49,9 @@ namespace WZIMopoly
 
             // A temporary code to add players to the game.
             var player1 = new PlayerModel("Player1", "Red");
-            var player2 = new PlayerModel("Player2", "Yellow");
+            var player2 = new PlayerModel("Player2", "Blue");
             var player3 = new PlayerModel("Player3", "Green");
-            var player4 = new PlayerModel("Player4", "Blue");
+            var player4 = new PlayerModel("Player4", "Yellow");
             Model.Players.Add(player1);
             Model.Players.Add(player2);
             Model.Players.Add(player3);
@@ -73,9 +73,9 @@ namespace WZIMopoly
             var positions = new List<Tuple<PlayerModel, Rectangle, GUIStartPoint>>()
             {
                 new(Model.Players[0], new Rectangle(0, 10, infoWidth, infoHeight), GUIStartPoint.TopLeft),
-                new(Model.Players[1], new Rectangle(0, 1070, infoWidth, infoHeight), GUIStartPoint.BottomLeft),
+                new(Model.Players[3], new Rectangle(0, 1070, infoWidth, infoHeight), GUIStartPoint.BottomLeft),
                 new(Model.Players[2], new Rectangle(1920, 1070, infoWidth, infoHeight), GUIStartPoint.BottomRight),
-                new(Model.Players[3], new Rectangle(1920, 10, infoWidth, infoHeight), GUIStartPoint.TopRight),
+                new(Model.Players[1], new Rectangle(1920, 10, infoWidth, infoHeight), GUIStartPoint.TopRight),
             };
 
             PlayerInfoModel model;
