8ce3f7d6906573abd364653b94560608b20ffd58
Wiktor Jaworski <wijaweq@gmail.com>
2023-05-20 22:36:58 +0200

    Add PlayerType enum

---
+++ WZIMopoly/Enums/PlayerType.cs
@@ -0,0 +1,28 @@
+﻿namespace WZIMopoly.Enums
+{
+    /// <summary>
+    /// Represents the player type.
+    /// </summary>
+    internal enum PlayerType
+    {
+        /// <summary>
+        /// The player plays locally.
+        /// </summary>
+        Local,
+
+        /// <summary>
+        /// The player plays over the network.
+        /// </summary>
+        Online,
+
+        /// <summary>
+        /// The player is a bot.
+        /// </summary>
+        Bot,
+
+        /// <summary>
+        /// The player is not set.
+        /// </summary>
+        None,
+    }
+}
