9f4d5f50f2ee07e3a6eca584345892cfe26579a3
Wiktor Jaworski <wijaweq@gmail.com>
2023-05-24 23:49:07 +0200

    Return button in lobby resets players

--- WZIMopoly/Controllers/LobbyScene/ReturnButtonController.cs
+++ WZIMopoly/Controllers/LobbyScene/ReturnButtonController.cs
@@ -1,4 +1,5 @@
-﻿using WZIMopoly.GUI.LobbyScene;
+﻿using WZIMopoly.Enums;
+using WZIMopoly.GUI.LobbyScene;
 using WZIMopoly.Models.LobbyScene;
 
 namespace WZIMopoly.Controllers.LobbyScene
@@ -18,6 +19,17 @@ namespace WZIMopoly.Controllers.LobbyScene
         /// The view of the return button.
         /// </param>
         public ReturnButtonController(ReturnButtonModel model, GUIReturnButton view)
-            : base(model, view) { }
+            : base(model, view)
+        {
+            OnButtonClicked += () =>
+            {
+                GameSettings.Players[0].ResetNick();
+                for (int i = 1; i <= 3; i++)
+                {
+                    GameSettings.Players[i].PlayerType = PlayerType.None;
+                    GameSettings.Players[i].ResetNick();
+                }
+            };
+        }
     }
 }
