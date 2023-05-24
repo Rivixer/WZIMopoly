27fd4269596f57572a47e1e16a64fe51e8752248
Wiktor Jaworski <wijaweq@gmail.com>
2023-05-24 23:43:00 +0200

    Add players list to lobby

--- WZIMopoly/Scenes/LobbyScene.cs
+++ WZIMopoly/Scenes/LobbyScene.cs
@@ -1,4 +1,4 @@
-using WZIMopoly.Controllers.LobbyScene;
+﻿using WZIMopoly.Controllers.LobbyScene;
 using WZIMopoly.GUI;
 using WZIMopoly.GUI.LobbyScene;
 using WZIMopoly.Models;
@@ -26,6 +26,7 @@ namespace WZIMopoly.Scenes
         /// <inheritdoc/>
         public override void Initialize()
         {
+            Model.InitializeChild<LobbyPlayersModel, GUILobbyPlayers, LobbyPlayersController>();
             Model.InitializeChild<ReturnButtonModel, GUIReturnButton, ReturnButtonController>();
             Model.InitializeChild<StartGameButtonModel, GUIStartGameButton, StartGameButtonController>();
             Model.InitializeChild<LocalModeButtonModel, GUILocalModeButton, LocalModeButtonController>();
