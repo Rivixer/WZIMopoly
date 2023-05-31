4b732b3801b9eb1d782086e0437a14d76a6c46af
Ktos1 <maty04188@gmail.com>
2023-06-01 00:10:10 +0200

    Delete IGUIDynamicPosition.cs

--- WZIMopoly/GUI/GameScene/GUIPawn.cs
+++ WZIMopoly/GUI/GameScene/GUIPawn.cs
@@ -7,25 +7,12 @@ namespace WZIMopoly.GUI.GameScene
     /// <summary>
     /// Represents a pawn view.
     /// </summary>
-    internal sealed class GUIPawn : GUITexture, IGUIDynamicPosition
+    internal class GUIPawn : GUITexture
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="GUIPawn"/> class.
         /// </summary>
         internal GUIPawn(PawnModel model)
             : base("Images/Pawns/Pawn" + model.Color, new Rectangle(0, 0, 30, 30), GUIStartPoint.Center) { }
-
-
-        /// <inheritdoc/>
-        public void UpdatePosition(Point point)
-        {
-            UpdateDefaultDestinationRect(this, point);
-        }
-
-        /// <inheritdoc/>
-        public void UpdatePosition(Vector2 vector)
-        {
-            UpdateDefaultDestinationRect(this, vector);
-        }
     }
 }
