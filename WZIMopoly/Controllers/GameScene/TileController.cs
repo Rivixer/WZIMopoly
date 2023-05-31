53826fb0a80158530a1c937f8705060f9c25766b
Ktos1 <maty04188@gmail.com>
2023-06-01 00:13:25 +0200

    Add TileController class with generic type for model and view

--- WZIMopoly/Controllers/GameScene/TileController.cs
+++ WZIMopoly/Controllers/GameScene/TileController.cs
@@ -52,4 +52,47 @@ namespace WZIMopoly.Controllers.GameScene
         protected TileController(M model, GUITile view)
             : base(model, view) { }
     }
+
+    /// <summary>
+    /// Represents a base class for a tile controller
+    /// with a specific model and view type.
+    /// </summary>
+    /// <typeparam name="M">
+    /// The model type.
+    /// </typeparam>
+    /// <typeparam name="V">
+    /// The view type.
+    /// </typeparam>
+    internal abstract class TileController<M, V> : TileController
+        where M : TileModel
+        where V : GUITile
+    {
+        /// <summary>
+        /// Gets the model of the tile.
+        /// </summary>
+        /// <value>
+        /// The specified model of the tile.
+        /// </value>
+        internal new M Model => (M)base.Model;
+
+        /// <summary>
+        /// Gets the view of the tile.
+        /// </summary>
+        /// <value>
+        /// The specidfied view of the tile.
+        /// </value>
+        internal new V View => (V)base.View;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TileController{M}"/> class.
+        /// </summary>
+        /// <param name="model">
+        /// The model of the tile.
+        /// </param>
+        /// <param name="view">
+        /// The view of the tile.
+        /// </param>
+        protected TileController(M model, V view)
+            : base(model, view) { }
+    }
 }
