b0daec7d9451c14200c075836d8a5e0a9326171e
Wiktor Jaworski <wijaweq@gmail.com>
2023-06-01 22:11:00 +0200

    Add LobbyCode to lobby

---
+++ WZIMopoly/Controllers/LobbyScene/LobbyCodeController.cs
@@ -0,0 +1,23 @@
+﻿using WZIMopoly.GUI.LobbyScene;
+using WZIMopoly.Models.LobbyScene;
+
+namespace WZIMopoly.Controllers.LobbyScene
+{
+    /// <summary>
+    /// Represents a controller for the lobby code.
+    /// </summary>
+    internal class LobbyCodeController : Controller<LobbyCodeModel, GUILobbyCode>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LobbyCodeController"/> class.
+        /// </summary>
+        /// <param name="model">
+        /// The model of the lobby code.
+        /// </param>
+        /// <param name="view">
+        /// The view of the lobby code.
+        /// </param>
+        public LobbyCodeController(LobbyCodeModel model, GUILobbyCode view)
+            : base(model, view) { }
+    }
+}
