b9fce0fc95368b80c4679deccac6145d89a8cc0b
EwaSzrajber <ewaszrajberyt@gmail.com>
2023-06-02 22:56:44 +0200

    Changed height and width of black mask in XML and changed code in class GUIUpgrade

--- WZIMopoly/GUI/GameScene/GUIUpgrade.cs
+++ WZIMopoly/GUI/GameScene/GUIUpgrade.cs
@@ -66,7 +66,7 @@ namespace WZIMopoly.GUI.GameScene
             foreach (TileController tile in _model.TileControllers)
             {
                 var pos = tile.View.Position;
-                var rect = new Rectangle(pos.X, pos.Y - 1, pos.Width + 1, pos.Height + 1);
+                var rect = new Rectangle(pos.X, pos.Y, pos.Width, pos.Height);
                 var texture = new GUITexture("Images/Black", rect, opacity: 0.5f);
                 _tileTextures.Add(texture);
             }
