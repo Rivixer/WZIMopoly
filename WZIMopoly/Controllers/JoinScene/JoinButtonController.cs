095114b9312806ed99263dccedf6417643dfc74c
Wiktor Jaworski <wijaweq@gmail.com>
2023-06-03 22:15:29 +0200

    Add JoinButton to JoinScene

---
+++ WZIMopoly/Controllers/JoinScene/JoinButtonController.cs
@@ -0,0 +1,23 @@
+﻿using WZIMopoly.GUI.JoinScene;
+using WZIMopoly.Models.JoinScene;
+
+namespace WZIMopoly.Controllers.JoinScene
+{
+    /// <summary>
+    /// Represents the join button controller.
+    /// </summary>
+    internal class JoinButtonController : ButtonController<JoinButtonModel, GUIJoinButton>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JoinButtonController"/> class.
+        /// </summary>
+        /// <param name="model">
+        /// The model of the join button.
+        /// </param>
+        /// <param name="view">
+        /// The view of the join button.
+        /// </param>
+        public JoinButtonController(JoinButtonModel model, GUIJoinButton view)
+            : base(model, view) { }
+    }
+}
