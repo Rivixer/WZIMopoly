366c5d6318cc6280b8fb568f6ec03257ee6c955e
Wiktor Jaworski <wijaweq@gmail.com>
2023-06-03 20:45:18 +0200

    Add more player types, add flags

--- WZIMopoly/Enums/PlayerType.cs
+++ WZIMopoly/Enums/PlayerType.cs
@@ -1,28 +1,46 @@
-﻿namespace WZIMopoly.Enums
+﻿using System;
+
+namespace WZIMopoly.Enums
 {
     /// <summary>
     /// Represents the player type.
     /// </summary>
+    [Flags]
     internal enum PlayerType
     {
         /// <summary>
+        /// The player is not set.
+        /// </summary>
+        None = 1 << 0,
+
+        /// <summary>
         /// The player plays locally.
         /// </summary>
-        Local,
+        Local = 1 << 1,
+
+        /// <summary>
+        /// The player plays over the network but is not the host.
+        /// </summary>
+        OnlinePlayer = 1 << 2,
+
+        /// <summary>
+        /// The player is the host of network game.
+        /// </summary>
+        OnlineHostPlayer = 1 << 3,
 
         /// <summary>
         /// The player plays over the network.
         /// </summary>
-        Online,
+        Online = OnlinePlayer | OnlineHostPlayer,
+
+        /// <summary>
+        /// The player is not a bot.
+        /// </summary>
+        NotBot = Local | Online,
 
         /// <summary>
         /// The player is a bot.
         /// </summary>
-        Bot,
-
-        /// <summary>
-        /// The player is not set.
-        /// </summary>
-        None,
+        Bot = 1 << 4,        
     }
 }
