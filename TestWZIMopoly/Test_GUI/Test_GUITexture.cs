7576c7025071dcaeb61bcfe601f10f1f0511b3d2
jacobkostek <s210879@sggw.edu.pl>
2023-06-05 23:26:45 +0200

    Added tests for GUITexture.

--- TestWZIMopoly/Test_GUI/Test_GUITexture.cs
+++ TestWZIMopoly/Test_GUI/Test_GUITexture.cs
@@ -1,13 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WZIMopoly.Enums;
+﻿using WZIMopoly.Enums;
 using WZIMopoly.GUI;
 using Microsoft.Xna.Framework;
-using System.Security.Cryptography.X509Certificates;
 using WZIMopoly.Engine;
 
 namespace TestWZIMopoly.Test_GUI
