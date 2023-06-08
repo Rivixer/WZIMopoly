54dadf555aaf7bef1b007d487b3fedccd0cc739d
Wiktor Jaworski <wijaweq@gmail.com>
2023-06-08 12:58:16 +0200

    Make UseElevator button visible only for client

--- WZIMopoly/Controllers/GameScene/GameSceneButtonControllers/UseElevatorButtonController.cs
+++ WZIMopoly/Controllers/GameScene/GameSceneButtonControllers/UseElevatorButtonController.cs
@@ -1,4 +1,5 @@
-﻿using WZIMopoly.GUI.GameScene.GUIGameSceneButtons;
+﻿using WZIMopoly.Attributes;
+using WZIMopoly.GUI.GameScene.GUIGameSceneButtons;
 using WZIMopoly.Models.GameScene.GameSceneButtonModels;
 
 namespace WZIMopoly.Controllers.GameScene.GameSceneButtonControllers
@@ -6,6 +7,7 @@ namespace WZIMopoly.Controllers.GameScene.GameSceneButtonControllers
     /// <summary>
     /// Represents the use elevator button controller.
     /// </summary>
+    [UpdatesNetwork]
     internal class UseElevatorButtonController : ButtonController<UseElevatorButtonModel, GUIUseElevatorButton>
     {
         /// <summary>
