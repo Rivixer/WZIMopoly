d69e1d31538c47481fd5beaeb3a14335af3a7928
Wiktor Jaworski <wijaweq@gmail.com>
2023-06-09 22:38:58 +0200

    Create chance card classes

---
+++ WZIMopoly/Controllers/GameScene/ChanceCardController.cs
@@ -0,0 +1,23 @@
+﻿using WZIMopoly.GUI.GameScene;
+using WZIMopoly.Models.GameScene;
+
+namespace WZIMopoly.Controllers.GameScene
+{
+    /// <summary>
+    /// Represents the chance card controller.
+    /// </summary>
+    internal class ChanceCardController : Controller<ChanceCardModel, GUIChanceCard>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChanceCardController"/> class.
+        /// </summary>
+        /// <param name="model">
+        /// The model of the chance card controller.
+        /// </param>
+        /// <param name="view">
+        /// The view of the chance card controller.
+        /// </param>
+        public ChanceCardController(ChanceCardModel model, GUIChanceCard view)
+            : base(model, view) { }
+    }
+}
