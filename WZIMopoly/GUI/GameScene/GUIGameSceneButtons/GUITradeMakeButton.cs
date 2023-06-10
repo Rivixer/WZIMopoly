f7a80f9cb021b507d26432d2a23a8fc0969a8960
Wiktor Jaworski <wijaweq@gmail.com>
2023-06-10 23:31:59 +0200

    Add MakeTrade button

---
+++ WZIMopoly/GUI/GameScene/GUIGameSceneButtons/GUITradeMakeButton.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Runtime.Serialization;
+using WZIMopoly.Enums;
+using WZIMopoly.Models.GameScene.GameButtonModels;
+
+namespace WZIMopoly.GUI.GameScene.GUIGameSceneButtons
+{
+    /// <summary>
+    /// Represents the make trade button view.
+    /// </summary>
+    internal sealed class GUITradeMakeButton : GUIButton<TradeMakeButtonModel>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GUITradeMakeButton"/> class.
+        /// </summary>
+        /// <param name="model">
+        /// The model of the make trade button.
+        /// </param>
+        internal GUITradeMakeButton(TradeMakeButtonModel model)
+            : base(model, new Rectangle(1060, 923, 256, 88), GUIStartPoint.Left, disableTexture: false)
+        {
+            SetButtonHoverArea(5, 0.8f);
+        }
+
+        /// <inheritdoc/>
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (GameSettings.CurrentPlayer.PlayerStatus == PlayerStatus.Trading)
+            {
+                base.Draw(spriteBatch);
+            }
+        }
+    }
+}
---
+++ WZIMopoly/Models/GameScene/GameSceneButtonModels/TradeMakeButtonModel.cs
@@ -0,0 +1,27 @@
+﻿using WZIMopoly.Enums;
+
+namespace WZIMopoly.Models.GameScene.GameButtonModels
+{
+    /// <summary>
+    /// Represents the make trade button model.
+    /// </summary>
+    internal sealed class TradeMakeButtonModel : ButtonModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TradeMakeButtonModel"/> class.
+        /// </summary>
+        internal TradeMakeButtonModel()
+            : base("MakeTrade") { }
+
+        public override void Update()
+        {
+            base.Update();
+            var player = GameSettings.CurrentPlayer;
+            IsActive = (WZIMopoly.GameType == GameType.Online && player == GameSettings.Client || WZIMopoly.GameType == GameType.Local)
+                && Conditions()
+                && (player.PlayerStatus == PlayerStatus.Trading
+                && player.PurchasedTiles.Count > 0
+                || player.PlayerStatus == PlayerStatus.Trading);
+        }
+    }
+}
