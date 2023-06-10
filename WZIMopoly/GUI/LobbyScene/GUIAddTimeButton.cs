b940d2181bd51fb93f13a0d33c557371ba7c6368
Damian Wujkowski <gewo556@gmail.com>
2023-06-10 06:40:38 +0200

    Create MVC to add time button

---
+++ WZIMopoly/GUI/LobbyScene/GUIAddTimeButton.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using WZIMopoly.Enums;
+using WZIMopoly.Models.LobbyScene;
+
+namespace WZIMopoly.GUI.LobbyScene
+{
+    /// <summary>
+    /// Represents the add time button view.
+    /// </summary>
+    internal class GUIAddTimeButton : GUIButton<AddTimeButtonModel>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GUIAddTimeButton"/> class.
+        /// </summary>
+        /// <param name="model">
+        /// The model of the add time button.
+        /// </param>
+        public GUIAddTimeButton(AddTimeButtonModel model)
+            : base(model, new Rectangle(1282, 700, 75, 75), GUIStartPoint.TopLeft, false, false)
+        {
+            SetButtonHoverArea(5, 0.75f);
+        }
+    }
+}
