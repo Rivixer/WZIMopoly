10dcc95516c4b15ca1b194dc71b9178d1ef68425
Damian Wujkowski <gewo556@gmail.com>
2023-06-10 06:38:53 +0200

    Create MVC to last not bankrupt button

---
+++ WZIMopoly/Controllers/LobbyScene/LastButtonController.cs
@@ -0,0 +1,46 @@
+﻿using WZIMopoly.Engine;
+using WZIMopoly.Enums;
+using WZIMopoly.GUI.LobbyScene;
+using WZIMopoly.Models.LobbyScene;
+
+namespace WZIMopoly.Controllers.LobbyScene
+{
+    /// <summary>
+    /// Represents the last not bankrupt button controller.
+    /// </summary>
+    internal class LastButtonController : ButtonController<LastButtonModel, GUILastButton>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LastButtonController"/> class.
+        /// </summary>
+        /// <param name="model">
+        /// The model of the last not bankrupt button button.
+        /// </param>
+        /// <param name="view">
+        /// The view of the last not bankrupt button button.
+        /// </param>
+        public LastButtonController(LastButtonModel model, GUILastButton view)
+            : base(model, view)
+        {
+            OnButtonClicked += () =>
+            {
+                GameSettings.gameEndType = GameEndType.LastNotBankrupt;
+            };
+        }
+
+        /// <inheritdoc/>
+        public override void Update()
+        {
+            Model.Update();
+
+            if (Model.Conditions()
+                && MouseController.WasLeftBtnClicked()
+                && View.IsHovered
+                && !Model.IsActive
+                && GameSettings.gameEndType == GameEndType.FirstBankruptcy)
+            {
+                OnClick();
+            }
+        }
+    }
+}
---
+++ WZIMopoly/Models/LobbyScene/LastButtonModel.cs
@@ -0,0 +1,22 @@
+﻿using WZIMopoly.Enums;
+
+namespace WZIMopoly.Models.LobbyScene
+{
+    /// <summary>
+    /// Represents the last not bankrupt button model.
+    /// </summary>
+    internal class LastButtonModel : ButtonModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LastButtonModel"/> class.
+        /// </summary>
+        public LastButtonModel()
+            : base("LobbyLast") { }
+
+        /// <inheritdoc/>
+        public override void Update()
+        {
+            IsActive = GameSettings.gameEndType == GameEndType.LastNotBankrupt;
+        }
+    }
+}
