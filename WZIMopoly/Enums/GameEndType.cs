2db2762670a19f58b9b3e85222bbb7439d1fd73e
Damian Wujkowski <gewo556@gmail.com>
2023-06-10 06:36:03 +0200

    Add enum with game end type

---
+++ WZIMopoly/Enums/GameEndType.cs
@@ -0,0 +1,18 @@
+﻿namespace WZIMopoly.Enums
+{
+    /// <summary>
+    /// Represents the game end type.
+    /// </summary>
+    internal enum GameEndType
+    {
+        /// <summary>
+        /// The first bankruptcy game type.
+        /// </summary>
+        FirstBankruptcy,
+
+        /// <summary>
+        /// The last not bankrupt game type.
+        /// </summary>
+        LastNotBankrupt,
+    }
+}
